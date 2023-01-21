37b2c1ec03f398490d8d0b7fb8d23626af8b35f1
Chanchal Soni <chanchalsoni1998@gmail.com>
2023-01-21 19:01:14 +0530

    day5

---
+++ com/src/OBS/Main.java
@@ -0,0 +1,76 @@
+package OBS;
+
+import java.util.Scanner;
+
+import com.masai.Dao.CustomerDao;
+import com.masai.Dao.CustomerImpl;
+import com.masai.exceptions.AccountantException;
+import com.masai.exceptions.CustomerException;
+import com.masai.usecases.RegisterCustomerUseCase2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		while (flag) {
+
+		System.out.println("=====================================\n"
+				+ "| Welcome to the Online Banking System:| "+"\n"
+				+"=====================================");
+		System.out.println("Choose the following Option:"+"\n"+"---------------------------------");
+		System.out.println("1.Log in as a Accountat");
+		System.out.println("2.Log in as a Customer");
+		System.out.println("3.New Customer? Please Register..!");
+		System.out.println("4.Exit"+"\n"+"---------------------------------");
+			try {
+				int choice = sc.nextInt();
+				switch (choice) {
+				case 1:
+					System.out.println("Enter username:");
+					String acUsername = sc.next();
+					System.out.println("Enter password:");
+					String acPassword = sc.next();
+
+					CustomerDao cdao = new CustomerImpl();
+					try {
+						cdao.loginAccountant(acUsername, acPassword);
+
+					} catch (AccountantException ae) {
+						System.out.println(ae.getMessage());
+					}
+
+					break;
+				case 2:
+					System.out.println("Enter username:");
+					String cUsername = sc.next();
+					System.out.println("Enter password:");
+					String cPassword = sc.next();
+
+					CustomerDao cdao1 = new CustomerImpl();
+					try {
+
+						cdao1.loginCustomer1(cUsername, cPassword);
+
+					} catch (CustomerException ce) {
+						System.out.println(ce.getMessage());
+					}
+					break;
+				case 3:
+					RegisterCustomerUseCase2.main1();
+
+					break;
+				case 4:
+					flag = false;
+					break;
+				default:
+					System.out.println("Invalid Entry...Please enter valid option 1,2 or 4");
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+		}
+	}
+
+}
---
+++ com/src/com/masai/usecases/RegisterCustomerUseCase2.java
@@ -0,0 +1,50 @@
+package com.masai.usecases;
+
+import java.util.Scanner;
+
+import com.masai.Dao.CustomerDao;
+import com.masai.Dao.CustomerImpl;
+import com.masai.bean.Customer;
+
+public class RegisterCustomerUseCase2 {
+
+	public static void main1() {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter username...");
+		String username = sc.next();
+
+		System.out.println("Enter Mobile Number...");
+		String mobile = sc.next();
+
+		System.out.println("Enter Password...");
+		String password = sc.next();
+
+		boolean flag = true;
+		while (flag) {
+
+			System.out.println("Enter Minimum balance to open account 10rs...");
+
+			int balance = sc.nextInt();
+			if (balance >= 10) {
+				flag=false;
+
+				Customer customer = new Customer();
+
+				customer.setUsername(username);
+				customer.setMobile(mobile);
+				customer.setPassword(password);
+				customer.setBalance(balance);
+
+				CustomerDao cdao = new CustomerImpl();
+
+				String message = cdao.registerCustomer(customer);
+
+				System.out.println(message);
+				System.out.println("Your account no is :" + customer.getAccNo());
+			}
+		}
+
+	}
+}
